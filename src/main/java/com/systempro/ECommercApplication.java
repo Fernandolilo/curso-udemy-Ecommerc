0c1eda57f08b28a27b34db5b292613b1f6ff1b6f
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-05-24 08:20:20 -0300

    Iniciando o projeto

---
+++ src/main/java/com/systempro/ECommercApplication.java
@@ -0,0 +1,13 @@
+package com.systempro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercApplication.class, args);
+	}
+
+}
