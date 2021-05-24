ad33f85b19aff9743c1a504f6f437a149812bacd
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-05-24 20:13:26 -0300

    Criando operacao de instanciacao

--- src/main/java/com/systempro/ECommercApplication.java
+++ src/main/java/com/systempro/ECommercApplication.java
@@ -1,13 +1,32 @@
 package com.systempro;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ECommercApplication {
+import com.systempro.domain.Categoria;
+import com.systempro.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ECommercApplication implements CommandLineRunner{
+
+	
+	@Autowired
+	CategoriaRepository categoriarepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ECommercApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");		
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
