f40c708434ad6874aa702291877faa813eb92e23
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-05-24 23:18:31 -0300

    Estado e Cidade, repository de ambos e inserido na base de dados

--- src/main/java/com/systempro/ECommercApplication.java
+++ src/main/java/com/systempro/ECommercApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.systempro.domain.Categoria;
+import com.systempro.domain.Cidade;
+import com.systempro.domain.Estado;
 import com.systempro.domain.Produto;
 import com.systempro.repositories.CategoriaRepository;
+import com.systempro.repositories.CidadeRepository;
+import com.systempro.repositories.EstadoRepository;
 import com.systempro.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class ECommercApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoReposytory;
+	
+	@Autowired
+	private EstadoRepository estadoReposytory;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ECommercApplication.class, args);
@@ -46,6 +56,18 @@ public class ECommercApplication implements CommandLineRunner{
 		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoReposytory.saveAll(Arrays.asList(p1, p2, p3));
 		
+		Estado est1 = new Estado(null, "São Paulo");
+		Estado est2 = new Estado(null, "Minas Gerais");
+		
+		Cidade c1 = new Cidade(null, "São Paulo", est1);
+		Cidade c2 = new Cidade(null, "Campinas", est1);
+		Cidade c3 = new Cidade(null, "Uberandia", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1, c2));
+		est2.getCidades().addAll(Arrays.asList(c3));
+		
+		estadoReposytory.saveAll(Arrays.asList(est1 , est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 
---
+++ src/main/java/com/systempro/domain/Estado.java
@@ -0,0 +1,95 @@
+package com.systempro.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Estado implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	
+	//mapeamento muitos para um no caso estamos mapeando estados e cidades
+	// como um estado tem muitas cidades, dentro da class Estado chamamos uma lista de cidades
+	// criando assim muitas cidades para um estado =>private List<Cidade> cidades = new ArrayList<>();
+	//@OneToMany(mappedBy = "estado") como na class cidade foi mapeado muitos para um, aqui esta sendo feito 
+	//um mapeamento reverso, en cidade mapeamos no atributo etado =>private Estado estado; por isto (mappedBy = "estado")
+	
+	@OneToMany(mappedBy = "estado")
+	private List<Cidade> cidades = new ArrayList<>();
+	
+	public Estado () {
+	}
+
+	// no contrutor com parametro não pode ser posto listas por este motivo
+	//private List<Cidade> cidades = new ArrayList<>(); não entra no construtor.
+	
+	public Estado(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+	
+	
+	
+}
