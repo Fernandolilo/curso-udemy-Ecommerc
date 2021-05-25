d2b4080255b964bc759130eea5b02dfd26293005
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-05-24 21:10:59 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/systempro/ECommercApplication.java
+++ src/main/java/com/systempro/ECommercApplication.java
@@ -8,14 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.systempro.domain.Categoria;
+import com.systempro.domain.Produto;
 import com.systempro.repositories.CategoriaRepository;
+import com.systempro.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ECommercApplication implements CommandLineRunner{
 
 	
 	@Autowired
-	CategoriaRepository categoriarepository;
+	private CategoriaRepository categoriarepository;
+	
+	@Autowired
+	private ProdutoRepository produtoReposytory;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ECommercApplication.class, args);
@@ -23,9 +28,24 @@ public class ECommercApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(null, "Informatica");
-		Categoria cat2 = new Categoria(null, "Escritorio");		
+		Categoria cat2 = new Categoria(null, "Escritorio");	
+		
+		Produto p1 = new Produto(null, "Computado", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoReposytory.saveAll(Arrays.asList(p1, p2, p3));
+		
 		
 	}
 
