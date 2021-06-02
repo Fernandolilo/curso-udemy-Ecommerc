98ebcc0889438cee009747868e23044f135ed0bd
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-06-01 22:22:53 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/systempro/config/DevConfig.java
@@ -0,0 +1,35 @@
+package com.systempro.config;
+
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.systempro.services.DBService;
+
+@Configuration
+@Profile("dev")
+
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String srategy;
+	
+	@Bean 
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(srategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
