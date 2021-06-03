aeb00d971f9ab764412d226fcbd25244bc0858c5
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-06-02 22:55:13 -0300

    Implementando toString de Pedido

--- src/main/java/com/systempro/domain/Pedido.java
+++ src/main/java/com/systempro/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.systempro.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -152,5 +155,27 @@ public class Pedido implements Serializable {
 			soma = soma + ip.getSubTotal();
 		}
 		return soma;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf =  NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número: ");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes:\n");
+		for(ItemPedido ip: getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor Total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
 	}
 }
