b0ae8a6d5e40846409f169944c4e13608ee1e523
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-06-04 17:30:28 -0300

    Implementado Autorização

--- src/main/java/com/systempro/config/SecurityConfig.java
+++ src/main/java/com/systempro/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.systempro.security.JWTAuthenticationFilter;
+import com.systempro.security.JWTAuthorizationFilter;
 import com.systempro.security.JWTUtil;
 
 @Configuration
@@ -61,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
