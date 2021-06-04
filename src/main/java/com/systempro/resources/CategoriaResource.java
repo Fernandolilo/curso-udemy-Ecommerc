540bf1bacb4b4f7b5b8eb3419c2d942327b31481
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-06-04 17:49:27 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/systempro/resources/CategoriaResource.java
+++ src/main/java/com/systempro/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
 		Categoria obj = service.fromDTO(objDto);
@@ -45,6 +47,7 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {
 		Categoria obj = service.fromDTO(objDto);
@@ -53,6 +56,7 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
@@ -71,14 +75,13 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
-	@RequestMapping(value= "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<CategoriaDTO>> findPage(
-			@RequestParam(value="page", defaultValue="0") Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage, 
-			@RequestParam(value="direction", defaultValue="ASC")String direction,
-			@RequestParam(value ="orderBy", defaultValue="nome")String orderBy) {
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy) {
 		// pega a lista de categorias
-		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction );
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
 		// converte a lista de categorias para um CategoriaDTO
 		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		// retorna apenas dados nocessários.
