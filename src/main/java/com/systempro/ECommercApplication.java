d0606b6aa1ee90fbe7f9cb3f0e0cdf13e15f3c6a
Fernando Silva <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-06-05 20:54:59 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/systempro/ECommercApplication.java
+++ src/main/java/com/systempro/ECommercApplication.java
@@ -1,19 +1,26 @@
 package com.systempro;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.systempro.services.S3Service;
+
 @SpringBootApplication
 public class ECommercApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ECommercApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\temp\\img\\imagem.png");
+		
 	}
 
 }
